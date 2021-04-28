dc69552482123fb107940e4f9a16d2d1d5504681
joyaljosescaria <joyaljosescaria1@gmail.com>
2021-04-29 01:29:00 +0530

    half login

--- api/controllers/student.js
+++ api/controllers/student.js
@@ -47,8 +47,7 @@ exports.registerStudent = async (req, res) => {
             res.status(400).json({ error: "Please provide a valid email address" })
         }
 
-        if(validate)
-        {
+        if (validate) {
 
         }
 
@@ -60,7 +59,7 @@ exports.registerStudent = async (req, res) => {
 
 // Submit Nomination
 
-exports.nomination = async (req,res) =>{
+exports.nomination = async (req, res) => {
     try {
         const nomination = {
             student_id: req.user.user_id,
@@ -68,10 +67,61 @@ exports.nomination = async (req,res) =>{
             election_id: req.body.election
         }
         const saveNomination = await nomination.save()
-        res.status(200).json({saveNomination})
+        res.status(200).json({ saveNomination })
     } catch (err) {
-        res.status(500).json({ error: err.message})
+        res.status(500).json({ error: err.message })
     }
 }
 
 
+// login
+
+exports.studentlogin = async (req, res) => {
+    try {
+        if (!req.body.email || !req.body.unique_id) {
+            res.status(400).json({ error: "Please provide the credentials" })
+        }
+        const login = await email.find({ email: req.body.email}).select(' email unique_id')
+        if(login[0].email===req.body.email){
+            if(login[0].unique_id ===req.body.unique_id)
+            {
+                const password = helper.generatePassword(8)
+
+                const salt = await bcrypt.genSalt(10)
+                const hashedPassword = await bcrypt.hash(password, salt)
+
+                const message = {
+                    from: 't.e.s.t.a.a.p.p.p@gmail.com', // Sender address
+                    to: login[0].email,         // List of recipients
+                    subject: 'Password for Login', // Subject line
+                    html: `Use <h1>${password}</h1> to login.` // Plain text body
+                };
+                transport.getSmpt().sendMail(message, function (err, info) {
+                    if (err) {
+                        console.log(err)
+                    } else {
+                        console.log(info);
+                    }
+                });
+
+                const data = {
+                    password: hashedPassword,
+                    pass_added_time: Date.now()
+                }
+
+                const saveLogin = await data.save()
+
+                res.status(200).json({ message:"Password sent successfully"})
+            }
+        }
+        else
+        {
+            res.status(403).json({ error: "provide correct credentials"})
+        }
+
+        
+
+    } catch (error) {
+        res.status(400).json({ error: err.message })
+    }
+}
